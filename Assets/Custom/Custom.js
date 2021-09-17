23c4c08191c87be4888dcebb1e63f8e28b23785e
git-perfortech <90724743+git-perfortech@users.noreply.github.com>
2021-09-17 13:24:00 +0300

    proje yüklemesi

---
+++ Assets/Custom/Custom.js
@@ -0,0 +1,154 @@
+var options = {
+    series: [44, 55, 13, 43, 22],
+    chart: {
+    width: 380,
+    type: 'polarArea',
+  },
+  labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+
+
+  
+  var options = {
+    series: [
+        {
+            name: "Desktops",
+            data: [10, 25, 55, 10, 89, 52, 78, 17, 18],
+        },
+        {
+            name: "Mobiles",
+            data: [30, 31, 32, 51, 49, 62, 69, 51, 50]
+        }  
+    ],
+    chart: {
+    height: 235,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: 'Product Trends by Month',
+    align: 'center'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'],
+      opacity: 1
+    },
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart2"), options);
+  chart.render();
+  
+
+
+
+  
+      
+  var options = {
+    series: [{
+      name: "Session Duration",
+      data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+    },
+    {
+      name: "Page Views",
+      data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+    },
+    {
+      name: 'Total Visits',
+      data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+    }
+  ],
+    chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: [5, 7, 5],
+    curve: 'straight',
+    dashArray: [0, 8, 5]
+  },
+  title: {
+    text: 'Page Statistics',
+    align: 'left'
+  },
+  legend: {
+    tooltipHoverFormatter: function(val, opts) {
+      return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
+    }
+  },
+  markers: {
+    size: 0,
+    hover: {
+      sizeOffset: 6
+    }
+  },
+  xaxis: {
+    categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+      '10 Jan', '11 Jan', '12 Jan'
+    ],
+  },
+  tooltip: {
+    y: [
+      {
+        title: {
+          formatter: function (val) {
+            return val + " (mins)"
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val + " per session"
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val;
+          }
+        }
+      }
+    ]
+  },
+  grid: {
+    borderColor: '#f1f1f1',
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart3"), options);
+  chart.render();
+
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PerfortechAdminLayoutSystem.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult SubProduct()
+        {
+            return View();
+        }
+
+        public ActionResult Attribute()
+        {
+            return View();
+        }
+        
+
+    }
+}
